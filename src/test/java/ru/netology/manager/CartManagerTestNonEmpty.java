2742d1bdc768c054c4859075b661bd4448761a6f
hYxA <zybu@live.ru>
2021-02-02 13:22:43 +0300

    doubling code

--- src/test/java/ru/netology/manager/CartManagerTestNonEmpty.java
+++ src/test/java/ru/netology/manager/CartManagerTestNonEmpty.java
@@ -27,4 +27,22 @@ public class CartManagerTestNonEmpty {
     }
 
 
+    @Test
+    public void shouldRemoveIfNotExist() {
+        CartManager manager = new CartManager();
+        int idToRemove = 4;
+        PurchaseItem first = new PurchaseItem(1,1,50,50,"first");
+        PurchaseItem second = new PurchaseItem(1,1,50,50,"second");
+        PurchaseItem third = new PurchaseItem(1,1,50,50,"third");
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        manager.removeById(idToRemove);
+
+        PurchaseItem[] actual = manager.getAll();
+        PurchaseItem[] expected = new PurchaseItem[]{third, second};
+
+        assertArrayEquals(expected, actual);
+    }
 }
